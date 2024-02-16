67126ff00df7f1f4ae94aa26a5f087de8e5422a8
Marcus <125326139+ProgrammerMarcus@users.noreply.github.com>
2024-02-16 09:57:37 +0100

    sorted cards and now draws from same deck

--- index.js
+++ index.js
@@ -1,34 +1,43 @@
 let attempts = 0;
 let successes = 0;
+let deck_id = "new"
 
 async function check(value, suit) {
-    const response = await fetch("https://deckofcardsapi.com/api/deck/new/draw/?count=1");
+    const response = await fetch(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=1`);
     const data = await response.json();
+    console.log(data)
     if (document.querySelector(".unknown")) {
         document.querySelector(".unknown").remove();
     }
     document.querySelector(".guess").insertAdjacentHTML("beforeend", `<img class="unknown" src=${data.cards[0].image} alt="A card.">`);
     document.querySelector(".result").innerText = value === data.cards[0].value && suit === data.cards[0].suit ? "Result: Correct" : "Result: Incorrect";
+    document.querySelector(".remaining").innerText = `Remaining: ${data.remaining}`
     attempts++;
     if (value === data.cards[0].value && suit === data.cards[0].suit) {
         successes++;
     }
     document.querySelector(".score").innerText = successes + "/" + attempts;
     document.querySelector(".box").classList.add("slide")
-    return { value: value === data.cards[0].value, suit: suit === data.cards[0].suit };
+    if (data.remaining === 0) {
+        deck_id = "new"
+    } else {
+        deck_id = data.deck_id
+    }
 }
 
 async function getOptions() {
-    const response = await fetch("https://deckofcardsapi.com/api/deck/new/draw/?count=52");
+    const response = await fetch(`https://deckofcardsapi.com/api/deck/new/draw/?count=52`);
     const data = await response.json();
     const target = document.querySelector(".cards");
-    for (card of data.cards) {
+    // target.replaceChildren()
+    const collator = new Intl.Collator([], {numeric: true});
+    for (card of data.cards.sort((a, b) => collator.compare(a.value, b.value))) {
         target.insertAdjacentHTML("beforeend", `<img class="card" data-value=${card.value} data-suit=${card.suit} src=${card.image} alt="A card." />`);
     }
 }
 
-getOptions();
+getOptions(52);
 
 document.querySelector(".cards").addEventListener("click", (e) => {
-    check(e.target.dataset.value, e.target.dataset.suit).then((a) => console.log(a));
+    check(e.target.dataset.value, e.target.dataset.suit);
 });
